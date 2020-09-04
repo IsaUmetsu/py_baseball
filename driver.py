e8df46001dc5f63ed82d04e1221658e2cdbed71a
Isamu Umetsu <isamu.umetsu@avinton.com>
2020-09-04 20:44:24 +0900

    changed driver path

--- driver.py
+++ driver.py
@@ -9,12 +9,12 @@ from selenium.webdriver.support import expected_conditions as EC
 def getChromeDriver():
     option = ChromeOptions()
     option.add_argument('--headless')
-    return webdriver.Chrome(executable_path="./driver/chromedriver", options=option)
+    return webdriver.Chrome(executable_path="/Users/IsamuUmetsu/dev/py_baseball/driver/chromedriver", options=option)
 
 def getFirefoxDriver():
     option = FirefoxOptions()
     option.add_argument('-headless')
-    return webdriver.Firefox(executable_path="./driver/geckodriver", options=option)
+    return webdriver.Firefox(executable_path="/Users/IsamuUmetsu/dev/py_baseball/driver/geckodriver", options=option)
 
 def waitUntilLoad(driver, elem):
     # WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)
